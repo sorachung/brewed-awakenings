3aeff3118df62241148f38aeeb75a5f7de376cff
sorachung <sorachung.src@gmail.com>
2021-11-29 14:44:23 -0600

    added eventListener for clicking on employees

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,31 @@
 import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.id.startsWith("employee")) {
+            const [,employeeId] = itemClicked.id.split("--")
+            let employeeName = ""
+            for (const employee of employees) {
+                if (employee.id === parseInt(employeeId)) {
+                    employeeName = employee.name
+                }
+            }
+            let productsSold = 0;
+            for (const order of orders) {
+                if (order.employeeId === parseInt(employeeId)) {
+                   productsSold += 1
+                }
+            }
+            window.alert(`${employeeName} sold ${productsSold} products.`)
+        }
+    }
+)
 
 export const Employees = () => {
     let html = "<ul>"
